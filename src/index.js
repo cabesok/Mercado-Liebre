84ecf92981652fdc34f983dea6dca9c79c94cc90
cabesok <cabesok@gmail.comm>
2022-11-28 19:54:34 -0300

    Updates

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("port",process.env.PORT || 3000);
+
+app.listen(app.get("port"),() => console.log("Server Start http://localhost:"+app.get("port")));
+
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+app.use(require("./routes/web"))
